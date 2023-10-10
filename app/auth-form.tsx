3b894bdff5f96bed0bc0b8a3a8633078af4ef044
Paige Hensley <paige@papermountain.org>
2023-10-10 12:13:15 -0400

    Reworked login flow to go login->home and to auto redirect to the homepage upon a successful login. Added school branding to login, account, and homepage. Updated homepage content to only reflact team information

---
+++ app/auth-form.tsx
@@ -0,0 +1,41 @@
+'use client'
+import { Auth } from '@supabase/auth-ui-react'
+import './globals.css'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { Database } from './database.types'
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation'
+
+
+export default function AuthForm() {
+  const router = useRouter();
+  const supabase = createClientComponentClient<Database>()
+
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        console.log('Auth event:', event);
+        if (event === 'SIGNED_IN') {
+          router.push('/home');
+        }
+      }
+    );
+
+    // return () => {
+    //   authListener.unsubscribe();
+    // };
+  }, []);
+
+  return (
+    <Auth
+      supabaseClient={supabase}
+      // view="magic_link"
+      appearance={{ theme: ThemeSupa }}
+      theme="dark"
+      showLinks={false}
+      providers={[]}
+      redirectTo="http://localhost:3000/auth/callback"
+    />
+  )
+}
